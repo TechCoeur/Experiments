9d63e512a6a97a222442b7efa9b5e9c764fdc26e
Jeanne Boon <jqboon@coeurfamilleholdings.com>
2018-04-01 12:03:17 +0800

    added experiment folder

---
+++ experiment/experiment/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace experiment
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
